db544d3d8cd4cd199d03b8c7a89edabdbd50a56f
imfatdong <lindongxiao44@gmail.com>
2021-04-22 22:12:40 +0800

    init

--- base/3-异常处理.py
+++ base/3-异常处理.py
@@ -8,3 +8,33 @@ ValueError
 TypeError
 FileNotFound
 """
+
+
+# 自定义异常
+"""
+class 类型名字(Exception):
+	def __init__(self, message):
+		super().__init__()
+		self.__message = message
+
+	def __str__(self):
+		return self.message
+"""
+
+class My_Exception(Exception):
+    def __init__(self,message):
+        self.__message = message
+
+    def __str__(self):
+        return self.__message
+try:
+    # raise My_Exception("自定义异常")
+    i = 5/0
+except ArithmeticError as e:
+    print("99")
+except Exception as e:
+    print(e)
+else:
+    print(123)
+finally:
+    print(5666)
