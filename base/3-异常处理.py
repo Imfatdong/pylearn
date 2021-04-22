b5850e6acdd11fbc6d5151a57fd03c844f4248bc
imfatdong <lindongxiao44@gmail.com>
2021-04-22 22:12:49 +0800

    init

--- base/3-异常处理.py
+++ base/3-异常处理.py
@@ -9,7 +9,6 @@ TypeError
 FileNotFound
 """
 
-
 # 自定义异常
 """
 class 类型名字(Exception):
@@ -21,15 +20,18 @@ class 类型名字(Exception):
 		return self.message
 """
 
+
 class My_Exception(Exception):
-    def __init__(self,message):
+    def __init__(self, message):
         self.__message = message
 
     def __str__(self):
         return self.__message
+
+
 try:
     # raise My_Exception("自定义异常")
-    i = 5/0
+    i = 5 / 0
 except ArithmeticError as e:
     print("99")
 except Exception as e:
